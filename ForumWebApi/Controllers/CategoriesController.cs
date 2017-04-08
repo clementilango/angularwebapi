771468f1aac48249247a3e6dbc50f57965172e5a
clementilango <iclementpaul@gmail.com>
2017-04-09 00:46:50 +0530

    webapi

---
+++ ForumWebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,44 @@
+﻿using ForumWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ForumWebApi.Controllers
+{
+    //[Authorize]
+    public class CategoriesController : ApiController
+    {
+        // GET api/values
+        public List<Category> Get()
+        {
+            var Category = new List<Category>();
+            return Category;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public string Post(Category category)
+        {
+            return "Book record added successfully";
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ForumWebApi/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForumWebApi.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    public string description { get; set; }
+public string position { get; set; }
+    }
+}
